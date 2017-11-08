665a821f00fa18cda3187c61dfbe82fb8208dc06
casperoostland <caspertje99@hotmail.com>
2017-11-08 19:02:25 +0100

    Final herkansing

--- js/story.js
+++ js/story.js
@@ -1,10 +1,10 @@
-function downloading(al) {
+function downloading(al) {  // Bron https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_progressbar_label_js
   var bar = document.getElementById('progressBar');
   var status = document.getElementById('status');
-  status.innerHTML = al + "%";
+  status.innerHTML = al + "%"; // Al is nu nul
   bar.value = al;
   al++;
-  var sim = setTimeout("downloading(" + al + ")", 1);
+  var sim = setTimeout("downloading(" + al + ")", 1); // al plus 100% vult helemaal
   if (al == 100) {
     status.innerHTML = "100%";
     bar.value = 100;
@@ -12,7 +12,7 @@ function downloading(al) {
     
   }
   var change = document.getElementById("download");
-  if (change.innerHTML == "Download") {
+  if (change.innerHTML == "Download") {// als de knop op download staat verander naar Open File
    change.innerHTML = "Open File";
    }
 }
